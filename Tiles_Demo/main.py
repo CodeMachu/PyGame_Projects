494576bc440c13fd949bd84979d2304086e711ff
Matthew <codemachu@gmail.com>
2022-05-08 22:58:08 -0400

    Sunday Night

--- Tiles_Demo/main.py
+++ Tiles_Demo/main.py
@@ -10,7 +10,7 @@ pygame.init()
 game_board.load_tiles(game_board_images_loc)
 
 # Update and Display Tiles
-def display_tiles():
+def update_tiles():
     game_board.draw_tiles(window)
     pygame.display.update()
 
@@ -27,12 +27,13 @@ def main():
         # Set Frame Rate
         clock.tick(FPS)
 
+        # Check if the Quit Button has been pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
         # Update Tiles
-        display_tiles()
+        update_tiles()
 
 
 #
